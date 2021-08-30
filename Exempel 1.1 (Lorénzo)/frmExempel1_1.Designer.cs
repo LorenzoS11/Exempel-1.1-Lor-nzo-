31cf4014a60f3d53e084e20aa1e2d9e60e3dba1a
LorénzoTLCP <63541098+LorenzoS11@users.noreply.github.com>
2021-08-30 21:06:43 +0200

    Add project files.

---
+++ Exempel 1.1 (Lorénzo)/frmExempel1_1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Exempel_1._1__Lorénzo_
+{
+    partial class frmExempel1_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "btnKör";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "tbxTal1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "tbxTal2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(132, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(110, 26);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "tbxSumma";
+            // 
+            // frmExempel1_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 296);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "frmExempel1_1";
+            this.Text = "btnKör";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Exempel 1.1 (Lorénzo)/frmExempel1_1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exempel_1._1__Lorénzo_
+{
+
+    /* Gjord av Lorenzo Somi 2021-08-30 */
+    public partial class frmExempel1_1 : Form
+    {
+        private object tbxTal2;
+        private object tbxTal1;
+
+        public frmExempel1_1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Omvandla till int
+            int tal1 = tbxTal1.Text;
+
+
+            //Addera
+            int summa = tal1 + tbxTal2.Text;
+
+
+            //Omvandla summa till sträng och visa svaret
+            textBox1.Text = summa.ToString();
+        }
+
+        private int ParseToInt(string text)
+        {
+            int tal = 0;
+
+            //Börja med värdet av entalet
+            int postionsvärde = 1;
+
+            //"Parsa" Texten och beräkna tal  //LS fixat negativa tal ö1.1a
+            for (int i = text.Length-1; i >= 0; i--)
+            {
+                int teckenvärde = text[i] - 48;
+                tal += teckenvärde * postionsvärde;
+                postionsvärde *= 10;
+            }
+            return tal;
+        }
+
+    }
